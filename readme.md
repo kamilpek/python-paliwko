ce74b4c66dd1713404014686e134b5088c938131
kamilpek <kamil.pek@gmail.com>
2015-05-14 14:14:24 +0200

    init commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+Projekt aplikacji pythonowej z wykorzystaniem relacyjnej bazy danych SQLite.
+Paliwko - kalkulator paliwa.
+
+Ostatnia aktualizacja: 14.05.2015
---
+++ ver1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import Tkinter
+fuel = Tkinter.Tk()
+fuel.title("Paliwko ver. 1.0")
+
+wysw = fuel.Entry(fuel, width = 50, bg = "white", font = "Helvetica 14 bold")
+
+#fuel.Button(fuel, text="OK", width= 10, height = 10, relief = button_style, command = action)
+
+fuel.mainloop()
